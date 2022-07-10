99e142907543a3cf8aed89375ea0d7b4c3c49f3b
Deech-Teach <76897436+Deech-Teach@users.noreply.github.com>
2022-07-10 13:00:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,43 +1,40 @@
-noseX = 0;
-noseY = 0;
+noseX=0;
+noseY=0;
 
-function preload(){
-clown_nose=loadImage('https://i.postimg.cc/YC7yQjsM/nose.png');
+function preload() {
+  clown_nose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
 }
 
-function setup(){
-    canvas = createCanvas(300, 300);
-     canvas.center();
-    video=createCapture(VIDEO);
-    video.size(300,300);
-    video.hide();
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
 
-    poseNet=ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
-
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
 
-function modelLoaded(){
-    console.log('PoseNet Is Initialized');
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
 }
 
 function gotPoses(results)
 {
-    if(results.length > 0) 
-    {
-         console.log(results);
-         noseX =results[0].pose.nose.x;
-         noseY =results[0].pose.nose.Y;
-          console.log("nose x = " + results[0].pose.nose.x);
-     console.log("nose y = " + results[0].pose.nose.y); 
-    }
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+  }
 }
 
-function draw(){
-    image(video,0,0,300,300)
-    image(cloun_nose,noseX,noseY,30,30);
+function draw() {
+  image(video, 0, 0, 300, 300);
+  image(clown_nose, noseX, noseY, 30, 30);
 }
 
-function take_snapshot(){
-    save('myFilterImage.png');
+function take_snapshot(){    
+  save('myFilterImage.png');
 }
